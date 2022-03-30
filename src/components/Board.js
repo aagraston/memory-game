a696a6c5b7d976cfdac498e6f22ca47bba89fb7c
aagraston <aagraston@gmail.com>
2022-03-30 15:02:50 -0700

    Finally! Got the page to load only once, data stored in array and rendered

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,60 +19,51 @@ function Board() {
   const imgArray = [img01, img02, img03, img04, img05, img06, img07, img08]
 
   //array of card data objects
-  const cardsData = []
+  const [cardsData, setCardsData] = useState([])
+
+  useEffect(() => {
+    setUpImages()
+  }, [])
+
+  const setUpImages = () => {
+    const tmpArray = []
+    imgArray.forEach((image, index) => {
+      const card = {
+        image: image,
+        ind: index,
+        clicked: false,
+        id: uniqid(),
+      }
+      tmpArray.push(card)
+    })
+
+    setCardsData(tmpArray)
+  }
 
   //set to original index
-  imgArray.forEach((image, index) => {
-    const card = {
-      image: image,
-      ind: index,
-      clicked: false,
-      id: uniqid(),
-    }
-    cardsData.push(card)
-  })
 
-  //set the card array, and the ability to set a new config for the card array
-  const [cardArray, setCardArray] = useState(
-    cardsData.map((card) => <Card key={uniqid()} card={card} />)
-  )
-
-  console.log(cardArray)
+  console.log(cardsData)
 
   //function passed to card to be triggered on click:
-  function clickedCard(withInd) {
+  const clickedCard = (withInd) => {
     //if card has been clicked trigger score reset
     //trigger reset of clicked value
   }
 
-  function toggleClicked(withInd) {
+  const toggleClicked = (withInd) => {
     //toggle the card that was clicked
   }
-
-  function shuffleCards() {
-    setCardArray((prevArray) => {
-      let newArray = []
-      let curInd = prevArray.length
-      let randInd = 0
-
-      while (curInd != 0) {
-        randInd = Math.floor(Math.random() * curInd)
-        curInd--
-        ;[cardArray[curInd], cardArray[randInd]] = [
-          cardArray[randInd],
-          cardArray[curInd],
-        ]
-      }
-    })
-  }
-
-  shuffleCards()
-
   //create card objects
 
   //board has an array of tiles that are created & shuffled here
 
-  return <div className="board-container">{cardArray}</div>
+  return (
+    <div className="board-container">
+      {cardsData.map((card) => (
+        <Card key={uniqid()} card={card} />
+      ))}
+    </div>
+  )
 }
 
 export default Board
