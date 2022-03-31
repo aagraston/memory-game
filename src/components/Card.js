322e8b2677b892521c421290074b119dc4c2670a
aagraston <aagraston@gmail.com>
2022-03-31 15:40:06 -0700

    detecting which card is clicked, performing clicked function

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Card(props) {
   //expect a cards data object
-  const { card } = props
+  const { card, clickedCard } = props
 
   return (
     <div className="card-container">
-      <img src={card.image} />
+      <img src={card.image} onClick={clickedCard.bind(this, card.id)} />
     </div>
   )
 }
