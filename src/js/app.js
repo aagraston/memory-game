cafb3238bcac1b995a5fe591d42a8788eddde665
aagraston <aagraston@gmail.com>
2022-03-31 15:50:56 -0700

    score tracking in main app, passed to board

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import ScoreBoard from '../components/ScoreBoard'
 import Board from '../components/Board'
 import Instructions from '../components/Instructions'
 
 function App() {
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
+
+  const attainPoint = () => {
+    setScore((prevScore) => {
+      return prevScore + 1
+    })
+  }
+
+  const resetScore = () => {
+    if (score > highScore) {
+      setHighScore(score)
+    }
+
+    setScore(0)
+  }
+
   return (
     <div className="app">
       <Header />
-      <ScoreBoard />
+      <ScoreBoard score={score} highScore={highScore} />
       <div className="app-main-section">
         <Instructions />
         <Board />
